9b24c4d04e48dba53d3d954fb3063fce4c9b51b9
Tony Messias <tonysm@hey.com>
2021-08-01 15:21:50 -0300

    Fix psalm

--- src/Attachables/AttachableContract.php
+++ src/Attachables/AttachableContract.php
@@ -9,7 +9,7 @@ interface AttachableContract
 {
     public static function fromNode(array $attachmentData, DOMElement $attachment): ?AttachableContract;
 
-    public function toDOMElement(DOMDocument $doc, DOMElement $attachable, bool $withContent = false): DOMElement;
+    public function toDOMElement(DOMDocument $document, DOMElement $attachable, bool $withContent = false): DOMElement;
 
     public function richTextRender(): string;
 
