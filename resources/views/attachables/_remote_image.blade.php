163af2548688d9ef6b747ed282f14b55af5dba10
Tony Messias <tonysm@hey.com>
2021-08-07 17:33:05 -0300

    One more attempt

--- resources/views/attachables/_remote_image.blade.php
+++ resources/views/attachables/_remote_image.blade.php
@@ -1,10 +1,13 @@
-<figure class="attachment attachment--preview" @if ($gallery ?? false) data-trix-attributes='{"presentation":"gallery"}' @endif>
-    <a href="{{ $remoteImage->url }}">
-        <img src="{{ $remoteImage->url }}" width="{{ $remoteImage->width }}" height="{{ $remoteImage->height }}" />
-        @if ($remoteImage->nodeCaption())
-            <figcaption class="attachment__caption">
-                {{ $remoteImage->nodeCaption() }}
-            </figcaption>
-        @endif
-    </a>
+<figure class="attachment attachment--preview attachment--{{ $remoteImage->extension() }}" @if ($gallery ?? false) data-trix-attributes='{"presentation":"gallery"}' @endif>
+    <img src="{{ $remoteImage->url }}" width="{{ $remoteImage->width }}" height="{{ $remoteImage->height }}" />
+    @if ($remoteImage->caption)
+        <figcaption class="attachment__caption">
+            {{ $remoteImage->caption }}
+        </figcaption>
+    @else
+        <figcaption class="attachment__caption">
+            <span class="attachment__name">{{ $remoteImage->filename }}</span>
+            <span class="attachment__size">{{ $remoteImage->filesize }}</span>
+        </figcaption>
+    @endif
 </figure>
--- src/Exceptions/RichTextException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Tonysm\RichTextLaravel\Exceptions;
-
-use RuntimeException;
-
-class RichTextException extends RuntimeException
-{
-}
