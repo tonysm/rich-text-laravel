a97dbc68c033bd2472d3c5a061f181bcce522a4a
Tony Messias <tonysm@hey.com>
2021-08-27 16:44:28 -0300

    Tweak config comments

--- config/rich-text-laravel.php
+++ config/rich-text-laravel.php
@@ -2,7 +2,14 @@
 
 return [
     /*
-     | The Rich Text model used for the rich text relationships.
+     |--------------------------------------------------------------------------
+     | Rich Text Model
+     |--------------------------------------------------------------------------
+     |
+     | When using the suggested database structure, all your Rich Text content will be
+     | stored in the same Database table. All interactions with that table happens
+     | using this Eloquent Model. You can override this if you really need to.
+     |
      */
     'model' => \Tonysm\RichTextLaravel\Models\RichText::class,
 ];
