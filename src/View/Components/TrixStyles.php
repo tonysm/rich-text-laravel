d75227d11fc81addd408af9f7df5900bf94aa7b0
Tony Messias <tonysm@hey.com>
2022-06-15 18:23:14 -0300

    Fix psalm

--- src/View/Components/TrixStyles.php
+++ src/View/Components/TrixStyles.php
@@ -6,6 +6,11 @@ use Illuminate\View\Component;
 
 class TrixStyles extends Component
 {
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\View\View|\Closure|string
+     */
     public function render()
     {
         return view('rich-text-laravel::components.core-styles');
