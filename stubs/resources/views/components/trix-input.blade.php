9966068817affc0f718d0a3090c39ae7d482ea63
Tony Messias <tonysm@hey.com>
2024-02-01 22:49:59 -0300

    Update trix-component

--- stubs/resources/views/components/trix-input.blade.php
+++ stubs/resources/views/components/trix-input.blade.php
@@ -10,5 +10,5 @@
 <trix-editor
     id="{{ $id }}"
     input="{{ $id }}_input"
-    {{ $attributes->merge(['class' => 'trix-content rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50']) }}
+    {{ $attributes->merge(['class' => 'trix-content border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm prose max-w-none dark:prose-strong:text-white dark:prose-blockquote:text-white dark:prose-h1:text-white dark:prose-a:text-white prose-pre:!bg-gray-400 prose-pre:!text-gray-900 prose-pre:!p-3 dark:prose-pre:!bg-gray-800 dark:prose-pre:!text-gray-200']) }}
 ></trix-editor>
