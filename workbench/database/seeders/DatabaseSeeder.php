477d901ac44c15eaa6689e08278d1e9876d715dd
Tony Messias <tonysm@hey.com>
2024-02-06 23:32:21 -0300

    Install Workbench

---
+++ workbench/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Workbench\Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
