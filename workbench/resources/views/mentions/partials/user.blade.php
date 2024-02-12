530c270ee762be169fb92b3150679cd3bb7f099c
Tony Messias <tonysm@hey.com>
2024-02-12 15:22:57 -0300

    wip

--- workbench/resources/views/mentions/partials/user.blade.php
+++ workbench/resources/views/mentions/partials/user.blade.php
@@ -1,6 +1,4 @@
 <span class="not-prose mx-0">
-    <a href="#">
-        <img src="{{ $user->picture_url }}" alt="{{ $user->name }}'s Avatar" class="inline-block object-cover w-5 h-5 rounded-full" />
-    </a>
+    <a href="#"><img src="{{ $user->picture_url }}" alt="{{ $user->name }}'s Avatar" class="inline-block object-cover w-5 h-5 rounded-full" /></a>
     <span>{{ $user->name }}</span>
 </span>
