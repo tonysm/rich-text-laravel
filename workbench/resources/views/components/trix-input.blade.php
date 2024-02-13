c6dcbcbb0d55d71a19a0c3b23b8ea62c20ce3661
Tony Messias <tonysm@hey.com>
2024-02-13 14:44:23 -0300

    Tweaks the styles

--- workbench/resources/views/components/trix-input.blade.php
+++ workbench/resources/views/components/trix-input.blade.php
@@ -1,16 +1,18 @@
 @props(['id', 'name', 'value' => ''])
 
-<input
-    type="hidden"
-    name="{{ $name }}"
-    id="{{ $id }}_input"
-    value="{{ $value }}"
-/>
+<div class="w-full bg-white border border-gray-300 relative border-gray-300 p-2 focus-within:ring-1 focus-within:border-indigo-500 focus-within:ring-indigo-500 rounded-md shadow-sm [&_trix-toolbar]:sticky [&_trix-toolbar]:top-0 [&_trix-toolbar]:-mx-2 [&_trix-toolbar]:px-4 [&_trix-toolbar]:bg-white [&_trix-toolbar]:border-b [&_trix-toolbar]:shadow-sm [&_trix-toolbar]:py-2 [&_trix-toolbar]:z-10">
+    <input
+        type="hidden"
+        name="{{ $name }}"
+        id="{{ $id }}_input"
+        value="{{ $value }}"
+    />
 
-<trix-editor
-    id="{{ $id }}"
-    input="{{ $id }}_input"
-    {{ $attributes->merge(['class' => 'trix-content bg-white border-gray-300 focus:ring-1 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) }}
-    data-controller="rich-text"
-    data-action="tribute-replaced->rich-text#addMention trix-attachment-add->rich-text#upload"
-></trix-editor>
+    <trix-editor {{ $attributes->merge([
+        'id' => $id,
+        'class' => 'trix-content w-full ring-0 outline-none border-0 px-1 pb-0 pt-2 !shadow-none',
+        'input' => "{$id}_input",
+        'data-controller' => 'rich-text',
+        'data-action' => 'tribute-replaced->rich-text#addMention trix-attachment-add->rich-text#upload'
+    ]) }}></trix-editor>
+</div>
