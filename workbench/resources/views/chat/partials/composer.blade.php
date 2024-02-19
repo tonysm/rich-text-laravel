00a7facfed64d295773b6c96f50c50183267ac37
Tony Messias <tonysm@hey.com>
2024-02-18 23:19:35 -0300

    Use rounded-3xl instead of rounded-full

--- workbench/resources/views/chat/partials/composer.blade.php
+++ workbench/resources/views/chat/partials/composer.blade.php
@@ -1,7 +1,7 @@
 <form action="{{ route('messages.store') }}" method="post" class="group" data-controller="composer">
     @csrf
 
-    <div class="flex text-lg items-center justify-between rounded-full border px-1 border-gray-400 bg-white focus-within:border-transparent focus-within:ring-4 group-data-[composer-show-toolbar-value=true]:rounded-lg group-data-[composer-show-toolbar-value=true]:items-end">
+    <div class="flex text-lg items-center justify-between rounded-3xl border px-1 border-gray-400 bg-white focus-within:border-transparent focus-within:ring-4 group-data-[composer-show-toolbar-value=true]:rounded-lg group-data-[composer-show-toolbar-value=true]:items-end">
         <div class="shrink-0 ml-1 group-data-[composer-show-toolbar-value=true]:mb-2">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 0 1-.923 1.785A5.969 5.969 0 0 0 6 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337Z" />
