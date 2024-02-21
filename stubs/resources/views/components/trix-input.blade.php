41566e4fe72d0da1c7866050c843a9fdd6e722f0
Tony Messias <tonysm@hey.com>
2024-02-20 22:34:27 -0300

    Tweaks stub component

--- stubs/resources/views/components/trix-input.blade.php
+++ stubs/resources/views/components/trix-input.blade.php
@@ -7,8 +7,14 @@
     value="{{ $value }}"
 />
 
+<trix-toolbar
+    class="[&_.trix-button]:bg-white [&_.trix-button.trix-active]:bg-gray-300"
+    id="{{ $id }}_toolbar"
+></trix-toolbar>
+
 <trix-editor
     id="{{ $id }}"
+    toolbar="{{ $id }}_toolbar"
     input="{{ $id }}_input"
-    {{ $attributes->merge(['class' => 'trix-content border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm prose max-w-none dark:prose-strong:text-white dark:prose-blockquote:text-white dark:prose-h1:text-white dark:prose-a:text-white prose-pre:!bg-gray-400 prose-pre:!text-gray-900 prose-pre:!p-3 dark:prose-pre:!bg-gray-800 dark:prose-pre:!text-gray-200']) }}
+    {{ $attributes->merge(['class' => 'trix-content border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:ring-1 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm dark:[&_pre]:!bg-gray-700 dark:[&_pre]:rounded dark:[&_pre]:!text-white']) }}
 ></trix-editor>
