1d9cdf31f2f82c8c8eba12095a8bd2e85ba519c2
Tony Messias <tonysm@hey.com>
2026-01-12 19:47:46 -0300

    Move converting attachments for editing to the LexxyEditor instead of inlining

--- src/Editor/LexxyEditor.php
+++ src/Editor/LexxyEditor.php
@@ -2,6 +2,8 @@
 
 namespace Tonysm\RichTextLaravel\Editor;
 
+use DOMElement;
+use Tonysm\RichTextLaravel\Attachment;
 use Tonysm\RichTextLaravel\Fragment;
 
 class LexxyEditor implements Editor
@@ -13,6 +15,16 @@ class LexxyEditor implements Editor
 
     public function asEditable(Fragment $fragment): Fragment
     {
-        return $fragment;
+        return $fragment->replace(Attachment::$SELECTOR, function (DOMElement $node) {
+            if (! $node->hasAttribute('url')) {
+                // TODO: We also need to render the normal attachments (those that don't rely on content attribute, but actually render inside the tag)...
+                $node->setAttribute('content', match (true) {
+                    $node->hasAttribute('content') => json_encode($node->getAttribute('content')),
+                    default => '',
+                });
+            }
+
+            return $node;
+        });
     }
 }
--- workbench/resources/views/components/lexxy-input.blade.php
+++ workbench/resources/views/components/lexxy-input.blade.php
@@ -1,15 +1,6 @@
 @props(['id', 'name', 'value' => '', 'acceptFiles' => true])
 
 <div class="w-full bg-white border border-gray-300 relative border-gray-300 px-2 focus-within:ring-1 focus-within:border-indigo-500 focus-within:ring-indigo-500 rounded-md shadow-sm">
-    <lexxy-editor {{ $attributes->merge(['id' => $id, 'name' => $name, 'value' => str_replace('rich-text-attachment', 'action-text-attachment', Tonysm\RichTextLaravel\Fragment::wrap($value ?? '')->replace(Tonysm\RichTextLaravel\Attachment::$SELECTOR, function (DOMElement $node) {
-        if (! $node->hasAttribute('url')) {
-            $node->setAttribute('content', match (true) {
-                $node->hasAttribute('content') => json_encode($node->getAttribute('content')),
-                default => '',
-            });
-        }
-
-        return $node;
-    })), 'class' => 'prose max-w-none']) }}></lexxy-editor>
+    <lexxy-editor {{ $attributes->merge(['id' => $id, 'name' => $name, 'value' => str_replace('rich-text-attachment', 'action-text-attachment', $value), 'class' => 'prose max-w-none']) }}></lexxy-editor>
 </div>
 
