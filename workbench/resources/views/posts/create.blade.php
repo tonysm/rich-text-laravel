b7848a497046b53aacd539238f8a97cc640b6f1e
Tony Messias <tonysm@hey.com>
2026-02-07 15:40:28 -0300

    Adds editor switcher to create form

--- workbench/resources/views/posts/create.blade.php
+++ workbench/resources/views/posts/create.blade.php
@@ -12,6 +12,7 @@
 
     <div class="mt-6 space-y-6">
        <div class="rounded border shadow p-6 space-y-2 bg-white">
+            @include('posts.partials.editor_switcher', ['editor' => $editor])
             @include('posts.partials.form', ['post' => null, 'editor' => $editor])
         </div>
     </div>
