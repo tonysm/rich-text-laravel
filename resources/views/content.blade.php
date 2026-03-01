e4c59e316de7f94719011068f3f4dfa3fd560f3b
Tony Messias <tonysm@hey.com>
2026-03-01 20:19:02 -0300

    Tweaks the wrapping class name

--- resources/views/content.blade.php
+++ resources/views/content.blade.php
@@ -1,5 +1,9 @@
-<div class="trix-content">
-@if (trim($trixContent = $content->renderWithAttachments()))
-    {!! $trixContent !!}
+<div @class([
+    'trix-content' => \Tonysm\RichTextLaravel\RichTextLaravel::editorName() === 'trix',
+    'lexxy-content' => \Tonysm\RichTextLaravel\RichTextLaravel::editorName() === 'lexxy',
+    \Tonysm\RichTextLaravel\RichTextLaravel::editorName() . '-content' => ! in_array(\Tonysm\RichTextLaravel\RichTextLaravel::editorName(), ['trix', 'lexxy']),
+])>
+@if (trim($content = $content->renderWithAttachments()))
+    {!! $content !!}
 @endif
 </div>
