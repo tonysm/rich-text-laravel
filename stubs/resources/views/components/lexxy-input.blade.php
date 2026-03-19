6496f8136248d8e16d1dc12aef94a42c6acbf551
Tony Messias <tonysm@hey.com>
2026-03-19 00:30:00 -0300

    Use lexxy-content class in the stubs input

--- stubs/resources/views/components/lexxy-input.blade.php
+++ stubs/resources/views/components/lexxy-input.blade.php
@@ -1,5 +1,5 @@
 @props(['id', 'name', 'value' => ''])
 
 <lexxy-editor
-    {{ $attributes->merge(['id' => $id, 'name' => $name, 'value' => $value]) }}
+    {{ $attributes->merge(['id' => $id, 'name' => $name, 'value' => $value, 'class' => 'lexxy-content']) }}
 >{{ $slot }}</lexxy-editor>
--- workbench/resources/views/components/lexxy-input.blade.php
+++ workbench/resources/views/components/lexxy-input.blade.php
@@ -1,7 +1,7 @@
 @props(['id', 'name', 'value' => '', 'acceptFiles' => true, 'mentions' => true, 'mentionsUrl' => null])
 
 <div class="w-full bg-white border border-gray-300 relative px-2 focus-within:ring-1 focus-within:border-indigo-500 focus-within:ring-indigo-500 rounded-md shadow-sm">
-    <lexxy-editor {{ $attributes->merge(['id' => $id, 'name' => $name, 'value' => $value]) }}>
+    <lexxy-editor {{ $attributes->merge(['id' => $id, 'name' => $name, 'value' => $value, 'class' => 'lexxy-content']) }}>
         @if ($mentions ?? false)
             <lexxy-prompt trigger="@" name="mention" src="{{ $mentionsUrl ?? route('mentions.index') }}"></lexxy-prompt>
         @endif
