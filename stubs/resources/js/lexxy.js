c9fed3068c6e840942ebb0824b290d94a4425a69
Tony Messias <tonysm@hey.com>
2026-03-18 22:56:54 -0300

    Fix the lexxy stub file

--- stubs/resources/js/lexxy.js
+++ stubs/resources/js/lexxy.js
@@ -1,8 +1,6 @@
-import * as Lexxy from "lexxy"
+import * as Lexxy from "@37signals/lexxy"
 
 // Rich Text Laravel uses a custom attachment tag name, so we must configure it here...
 Lexxy.configure({ global: { attachmentTagName: "rich-text-attachment" } })
 
-window.Trix = Trix;
-
-export default Trix;
+export default Lexxy;
