fa9b41a85fb85d34be7cb81cfc3f18a64aebf7b2
Tony Messias <tonysm@hey.com>
2026-03-18 22:41:59 -0300

    Adds the ESM files for trix and lexxy to the build (without hashing them)

--- scripts/build.js
+++ scripts/build.js
@@ -5,12 +5,18 @@ let esbuild = require('esbuild')
 
 let distDir = path.resolve(__dirname, '../resources/dist')
 
-let files = {
+let hashedFiles = {
     'node_modules/@37signals/lexxy/dist/stylesheets/lexxy.css': 'lexxy.css',
     'node_modules/trix/dist/trix.css': 'trix.css',
     'resources/css/lexxy-rich-text-laravel-attachments.css': 'lexxy-rich-text-laravel-attachments.css',
     'resources/css/trix-rich-text-laravel.css': 'trix-rich-text-laravel.css',
     'resources/css/trix-rich-text-laravel-attachments.css': 'trix-rich-text-laravel-attachments.css',
+}
+
+// We'll ship these files in ESM for Importmaps Laravel...
+let esmFiles = {
+    'node_modules/@37signals/lexxy/dist/lexxy.esm.js': 'lexxy.esm.js',
+    'node_modules/trix/dist/trix.esm.js': 'trix.esm.js',
 }
 
 if (fs.existsSync(distDir)) {
@@ -22,7 +28,7 @@ fs.mkdirSync(distDir, { recursive: true })
 async function build() {
     let manifest = {}
 
-    for (let [src, dest] of Object.entries(files)) {
+    for (let [src, dest] of Object.entries(hashedFiles)) {
         let outfile = path.join(distDir, dest)
 
         await esbuild.build({
@@ -40,6 +46,21 @@ async function build() {
 
         fs.renameSync(outfile, path.join(distDir, hashedName))
         manifest[`/${dest}`] = `/vendor/rich-text-laravel/${hashedName}`
+    }
+
+    for (let [src, dest] of Object.entries(esmFiles)) {
+        let outfile = path.join(distDir, dest)
+
+        await esbuild.build({
+            entryPoints: [src],
+            outfile,
+            bundle: true,
+            minify: true,
+            format: 'esm',
+            external: ['@rails/activestorage'],
+        })
+
+        manifest[`/${dest}`] = `/vendor/rich-text-laravel/${dest}`
     }
 
     fs.writeFileSync(path.join(distDir, 'manifest.json'), JSON.stringify(manifest, null, 2) + '\n')
